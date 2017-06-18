b1fa3ef30023da02afae65f3afdd9432ffa25300
Diobert Dioneo <diobertbd@singtel.com>
2017-06-18 22:42:10 +0800

    delete cli option for bash script

--- basics/scripts/sample.sh
+++ basics/scripts/sample.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-aws cloudformation create-stack --stack-name lessons-basic-sample-stack \
+if [ "$1" == "delete" ]; then
+  aws cloudformation delete-stack --stack-name lessons-basic-sample-stack
+else
+  aws cloudformation create-stack --stack-name lessons-basic-sample-stack \
 				--template-body file://../templates/sample.yaml
+fi
