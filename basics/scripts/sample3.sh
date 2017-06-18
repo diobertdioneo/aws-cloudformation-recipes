893ddb0fbf32c89986e6b88a699bf305b64980a1
Diobert Dioneo <diobertbd@singtel.com>
2017-06-19 01:41:50 +0800

    A sample template with parameterized values
    - bash script that passes parameter key-value pairs to the cloud formation template;
    - cloud formation template that uses parameters for most of the resource properties

---
+++ basics/scripts/sample3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ "$1" == "delete" ]; then
+  echo "deleting stack lessons-basic-sample2-stack"
+  aws cloudformation delete-stack --stack-name lessons-basic-sample2-stack
+elif [ "$1" == "validate" ]; then
+  echo "validating stack lessons-basic-sample2-stack"
+  aws cloudformation validate-template --template-body file://../templates/sample2.yaml
+elif [ "$1" == "create" ]; then
+  echo "creating stack lessons-basic-sample2-stack"
+  aws cloudformation create-stack --stack-name lessons-basic-sample2-stack \
+                                  --template-body file://../templates/sample2.yaml \
+				  --parameters ParameterKey=ParamImageId,ParameterValue=ami-fc5ae39f \
+				  ParameterKey=ParamKeyName,ParameterValue=lessons \
+				  ParameterKey=ParamInstanceType,ParameterValue=t2.micro \
+				  ParameterKey=ParamTagNameValue,ParameterValue=lessons-basic-myec2instance \
+				  ParameterKey=ParamTagGroupValue,ParameterValue=lessons \
+				  ParameterKey=ParamAvailabilityZone,ParameterValue=ap-southeast-1a \
+				  ParameterKey=ParamVolumeSize,ParameterValue=20 \
+else
+  echo "usage:"
+  echo "  $0 validate"
+  echo "  $0 create"
+  echo "  $0 delete"
+fi
